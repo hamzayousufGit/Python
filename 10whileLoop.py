563a639b51dd7f16921575bf871ed2baca3134b5
Hamza Yousuf GIT <roxerhamza@gmail.com>
2026-06-01 09:00:39 +0000

    commited

---
+++ 10whileLoop.py
@@ -0,0 +1,28 @@
+#While Loops, it means that run a code until a condition is true, stop the loop when condition is false. we have to make a variable to use in condition
+
+#Example 1
+i = 0
+while(i <= 10):
+    print(i)
+    i = i + 1
+
+print("While Loop 1, finished")
+
+#Example 2
+j = 5
+while(j >= 0):
+    print(j)
+    j = j - 1
+
+print("While Loop 2, finished")
+
+#Do While-Loop
+#This is not in python but we have to know about it 
+
+#What does it do?
+#It runs the code(least which has to be repeated) at least 1 time, weather the condition is true or not
+
+#Syntax
+#do{
+# code
+# }(condition)
