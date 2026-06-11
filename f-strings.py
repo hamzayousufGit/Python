7596c94a85042b0637b6310c3562444fb0852f2a
Hamza Yousuf GIT <roxerhamza@gmail.com>
2026-06-11 07:00:42 +0000

    commit

---
+++ f-strings.py
@@ -0,0 +1,14 @@
+#F-strings, these can help you to use variables in a string, we use {} to use variable
+
+#Example 1
+name = "Python"
+lastName = "Programming"
+
+fullName = f"{name} {lastName}"
+print(fullName)
+
+#Example 2
+favFood = "burger"
+name = "pythonPro"
+
+print(f"my name is {name} and my favourite food is {favFood}")
