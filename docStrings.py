58f23ad1544a8f1fc3afd1a88d0a6783f3d8e782
Hamza Yousuf GIT <roxerhamza@gmail.com>
2026-06-11 07:31:45 +0000

    initial commit

---
+++ docStrings.py
@@ -0,0 +1,10 @@
+#DocStrings, these help in telling someone about this function, loop or element etc
+
+def addTwoNumbers(a, b):
+    """
+        This function is for adding two numbers,
+        with taking two parameters
+    """#doc strings, are always on the first line, logic is written after doc strings
+    print(a + b)
+
+addTwoNumbers(22, 45)
