c3564794e9afdc1c20057bd2ee1456d8152c4dac
Hamza Yousuf GIT <roxerhamza@gmail.com>
2026-06-18 06:36:04 +0000

    initial commit

---
+++ 20sets.py
@@ -0,0 +1,9 @@
+#Set, A set is a collection that does not allow duplicate values and prints values unordered.
+
+fruits = {"apple", "banana", "strawberry", "mango", "orange", "pineapple", "cherry"}
+print(fruits)
+
+#if we add duplicate values it will delete one value from the both
+
+cars = {"lamborghini", "bugatti", "supra", "F1 racing car", "toyota", "lamborghini"}
+print(cars)
